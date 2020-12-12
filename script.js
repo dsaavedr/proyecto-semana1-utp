172c79d12d6e1223f7c508b2c0b500bc7531ba82
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-12-12 18:40:46 -0500

    Rehacer JSON, finalizar importación y consumo por parte del componente.

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+// Crear aplicación de Vue
+
 // Crear el componente de Vue para usar en la página.
 
 Vue.component("member", {
     template: `
-    <div class="col-md-3 col-sm-6 member">
-        <img src="https://via.placeholder.com/150" alt="imagen-integrante">
-        <h3 class="text-center">{{nombre}}</h3>
-        <p class="text-center">{{descripcion}}</p>
+    <div class="col-md-6 col-lg-3 member">
+        <img :src="member.image" alt="imagen-integrante">
+        <h3 class="text-center">{{member.nombre}}</h3>
+        <h4 class="text-center">{{member.rol}}</h4>
+        <p class="text-center">{{member.descripcion}}</p>
     </div>
-    `
+    `,
+    props: ["member"]
 });
 
-const team = new Vue({
-    el: "#team",
-    data: {},
-    methods: {}
-});
+// Importar información de integrantes
+
+let members;
+
+const call = async query => {
+    const response = await fetch(query);
+    const data = await response.json();
+
+    return data;
+};
+
+call("./team.json")
+    .then(data => {
+        const team = new Vue({
+            el: "#team",
+            data: {
+                members: data
+            }
+        });
+    })
+    .catch(err => console.error(err));
