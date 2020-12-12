4074f05d7836418f670244e9bcbf4033bd925592
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-12-12 18:53:25 -0500

    Agregar contenedor a cada instancia para fingir margen

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let team;
 
 Vue.component("member", {
     template: `
-    <div class="col-md-6 col-lg-3 member">
-        <img :src="member.image" alt="imagen-integrante">
-        <h3 class="text-center">{{member.nombre}}</h3>
-        <h4 class="text-center">{{member.rol}}</h4>
-        <p class="text-center">{{member.descripcion}}</p>
+    <div class="col-md-6 col-lg-3">
+        <div class="col-12 member">
+            <img :src="member.image" alt="imagen-integrante">
+            <h3 class="text-center">{{member.nombre}}</h3>
+            <h4 class="text-center">{{member.rol}}</h4>
+            <p class="text-center">{{member.descripcion}}</p>
+        </div>
     </div>
     `,
     props: ["member"]
